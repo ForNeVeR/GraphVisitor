2ae25e3f61fce9ee377e595ddeb30815209510bb
ForNeVeR <neverthness@gmail.com>
2012-02-16 20:52:45 +0700

    routeGetter renamed to routeSelector. Thanks, nagg.

--- GraphVisitor/GraphVisitor/GraphVisitor.cs
+++ GraphVisitor/GraphVisitor/GraphVisitor.cs
@@ -7,27 +7,27 @@ namespace GraphVisitor
     internal static class GraphVisitor
     {
         public static IEnumerable<IEnumerable<TNode>> VisitGraph<TNode>(
-            IEnumerable<TNode> startNodes, TNode endNode, Func<TNode, IEnumerable<TNode>> routeGetter)
+            IEnumerable<TNode> startNodes, TNode endNode, Func<TNode, IEnumerable<TNode>> routeSelector)
         {
             var routes = new List<IEnumerable<TNode>>();
 
             foreach (var startNode in startNodes)
             {
-                routes.AddRange(TraverseRoute(new[] {startNode}, endNode, routeGetter));
+                routes.AddRange(TraverseRoute(new[] {startNode}, endNode, routeSelector));
             }
 
             return routes;
         }
 
         private static IEnumerable<IEnumerable<TNode>> TraverseRoute<TNode>(IEnumerable<TNode> route,
-            TNode endNode, Func<TNode, IEnumerable<TNode>> routeGetter)
+            TNode endNode, Func<TNode, IEnumerable<TNode>> routeSelector)
         {
             var routeList = route.ToList();
             var lastNode = routeList.Last();
 
             var result = new List<IEnumerable<TNode>>();
 
-            foreach (var path in routeGetter(lastNode))
+            foreach (var path in routeSelector(lastNode))
             {
                 if (routeList.Contains(path))
                 {
@@ -42,7 +42,7 @@ namespace GraphVisitor
                 }
                 else
                 {
-                    result.AddRange(TraverseRoute(runningRoute, endNode, routeGetter));
+                    result.AddRange(TraverseRoute(runningRoute, endNode, routeSelector));
                 }
             }
 
