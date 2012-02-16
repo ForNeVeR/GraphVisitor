07b1425d85d6f810c20a5faa8a499286de5a1447
ForNeVeR <neverthness@gmail.com>
2012-02-16 20:24:28 +0700

    Initial version.

---
+++ GraphVisitor/GraphVisitor/GraphVisitor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GraphVisitor
+{
+    internal static class GraphVisitor
+    {
+        public static IEnumerable<IEnumerable<TNode>> VisitGraph<TNode>(
+            IEnumerable<TNode> startNodes, TNode endNode, Func<TNode, IEnumerable<TNode>> routeGetter)
+        {
+            var routes = new List<IEnumerable<TNode>>();
+
+            foreach (var startNode in startNodes)
+            {
+                routes.AddRange(TraverseRoute(new[] {startNode}, endNode, routeGetter));
+            }
+
+            return routes;
+        }
+
+        private static IEnumerable<IEnumerable<TNode>> TraverseRoute<TNode>(IEnumerable<TNode> route,
+            TNode endNode, Func<TNode, IEnumerable<TNode>> routeGetter)
+        {
+            var routeList = route.ToList();
+            var lastNode = routeList.Last();
+
+            var result = new List<IEnumerable<TNode>>();
+
+            foreach (var path in routeGetter(lastNode))
+            {
+                if (routeList.Contains(path))
+                {
+                    continue;
+                }
+
+                var runningRoute = routeList.Concat(new[] {path});
+
+                if (path.Equals(endNode))
+                {
+                    result.Add(runningRoute);
+                }
+                else
+                {
+                    result.AddRange(TraverseRoute(runningRoute, endNode, routeGetter));
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ GraphVisitor/GraphVisitor/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GraphVisitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintRoutes(Test1());
+            Console.WriteLine();
+            PrintRoutes(Test2());
+        }
+
+        static void PrintRoutes<T>(IEnumerable<IEnumerable<T>> routes)
+        {
+            foreach (var route in routes)
+            {
+                Console.WriteLine(string.Join(" -> ", route));
+            }
+        }
+
+        static IEnumerable<IEnumerable<int>> Test1()
+        {
+            /*
+             * 1     2
+             *  \   /
+             *   v v
+             *    3
+             *    |
+             *    v
+             *    4
+             */
+            var pathDictionary = new Dictionary<int, int>
+                {
+                    {1, 3},
+                    {2, 3},
+                    {3, 4}
+                };
+
+            var routes = GraphVisitor.VisitGraph(new[] { 1, 2 }, 4, node => new[] { pathDictionary[node] });
+
+            return routes;
+            /* 
+             * Output:
+             * 1 -> 3 -> 4
+             * 2 -> 3 -> 4
+             */
+        }
+
+        static IEnumerable<IEnumerable<int>> Test2()
+        {
+            /*
+             * 1 --> 2
+             * |^   /|
+             * | \ v |
+             * |  3  |
+             * |  |  |
+             * |  v  |
+             * `->4<-'
+             *    |
+             *    v
+             *    5
+             */
+            var pathDictionary = new Dictionary<int, int[]>
+                {
+                    {1, new[]{2, 4}},
+                    {2, new[]{3, 4}},
+                    {3, new[]{4, 1}},
+                    {4, new[]{5}}
+                };
+
+            var routes = GraphVisitor.VisitGraph(new[] { 1, 2 }, 5, node => pathDictionary[node]);
+
+            return routes;
+            /* 
+             * Output:
+             * 1 -> 2 -> 3 -> 4 -> 5
+             * 1 -> 2 -> 4 -> 5
+             * 1 -> 4 -> 5
+             * 2 -> 3 -> 4 -> 5
+             * 2 -> 3 -> 1 -> 4 -> 5
+             * 2 -> 4 -> 5
+             */
+        }
+    }
+}
